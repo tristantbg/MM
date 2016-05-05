dbe49e2862fd72b5384aa527975296a5b3a35161
tristanb <tristan.bagot@gmail.com>
2016-05-05 14:55:36 +0200

    fix jquery fallback

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -4,7 +4,7 @@
 
 	<meta charset="UTF-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 	<title><?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?></title>
 	<meta name="description" content="<?php echo $site->description()->html() ?>">
 	<meta name="keywords" content="<?php echo $site->keywords()->html() ?>">
@@ -12,7 +12,6 @@
 	<meta property="og:image" content="<?= url('assets/images/og_image.jpg') ?>"/>
 	<link rel="shortcut icon" href="<?= url('assets/images/favicon.ico') ?>">
 	<link rel="icon" href="<?= url('assets/images/favicon.ico') ?>" type="image/x-icon">
-	<meta name="viewport" content="width=device-width,initial-scale=1.0">
 
 	<?php 
 	echo css('assets/css/app.min.css');
@@ -20,7 +19,7 @@
 	?>
 	
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-	<script>window.jQuery || document.write('<script src="assets/js/vendor/jquery.min.js">\x3C/script>')</script>
+	<script>window.jQuery || document.write('<script src="<?= url('assets/js/vendor/jquery.min.js') ?>">\x3C/script>')</script>
 
 	<?php if(!$site->customcss()->empty()): ?>
 		<style type="text/css">
