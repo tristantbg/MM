a99a03b6a1efed8000c644603a00efbcd09e7209
tristanb <tristan.bagot@gmail.com>
2016-05-11 20:39:45 +0200

    SEO + og:image fix

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -4,12 +4,55 @@
 
 	<meta charset="UTF-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<link rel="dns-prefetch" href="//www.google-analytics.com">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-	<title><?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?></title>
+	<link rel="canonical" href="<?php echo html($page->url()) ?>" />
+	<?php if($page->isHomepage()): ?>
+      <title><?php echo $site->title()->html() ?></title>
+    <?php else: ?>
+      <title><?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?></title>
+    <?php endif ?>
 	<meta name="description" content="<?php echo $site->description()->html() ?>">
+	<meta name="robots" content="index,follow" />
 	<meta name="keywords" content="<?php echo $site->keywords()->html() ?>">
-	<meta property="og:url" content="<?php echo $site->homePage()->url() ?>"/>
-	<meta property="og:image" content="<?= url('assets/images/og_image.jpg') ?>"/>
+	<meta name="DC.Title" content="<?php echo $page->title()->html() ?>" />
+    <meta name="DC.Description" content="<?php echo $site->description()->html() ?>"/ >
+    <?php if($page->isHomepage()): ?>
+      <meta property="og:title" content="<?php echo $site->title()->html() ?>" />
+    <?php else: ?>
+      <meta property="og:title" content="<?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?>" />
+    <?php endif ?>
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="<?php echo html($page->url()) ?>" />
+
+    <?php if($page->isHomepage()): ?>
+    <?php foreach($page->featuredimages()->toStructure()->flip() as $index => $media): ?>
+    <?php if($media->_fieldset() == 'image'):?>
+		<?php if($media->imagefile()->toFile() !== null):?>
+			<?php $image = $media->imagefile()->toFile()->url(); ?>
+			<meta property="og:image" content="<?php echo $image ?>" />
+		<?php endif ?>
+	<?php endif ?>
+	<?php endforeach ?>
+      
+    <?php elseif($page->content()->name() == "project"): ?>
+	      <?php foreach($page->medias()->toStructure()->flip() as $index => $media): ?>
+	      	<?php if($index < 4):?>
+	    	<?php if($media->_fieldset() == 'image'):?>
+			<?php if($media->imagefile()->toFile() !== null):?>
+				<?php $image = $media->imagefile()->toFile()->resize(1200)->url(); ?>
+				<meta property="og:image" content="<?php echo $image ?>" />
+			<?php endif ?>
+		<?php endif ?>
+		<?php endif ?>
+		<?php endforeach ?>
+	<?php elseif($page->content()->name() == "category"): ?>
+		<meta property="og:image" content="<?php echo $page->children()->visible()->flip()->first()->featuredimage()->toFile()->url() ?>" />
+    <?php endif ?>
+
+    <meta property="og:description" content="<?php echo $site->description()->html() ?>" />
+    <meta itemprop="name" content="<?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?>">
+    <meta itemprop="description" content="<?php echo $site->text()->html() ?>">
 	<link rel="shortcut icon" href="<?= url('assets/images/favicon.ico') ?>">
 	<link rel="icon" href="<?= url('assets/images/favicon.ico') ?>" type="image/x-icon">
 
